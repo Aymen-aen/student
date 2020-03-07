0d05ef1a3d0f2ec2a5206ea4afdd4043c826f93e
Aymen Khalfaoui <aymen.khalfaoui@gmail.com>
2020-03-07 22:31:35 +0100

    first init

---
+++ resources/views/studentslist.blade.php
@@ -0,0 +1,46 @@
+
+<div class="card mb-3">
+    <img src="https://cleaedu.com/wp-content/uploads/2018/12/574f1ac03ab14-750x480.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h2 class="card-title">LIST OF STUDENTS</h2>
+        <p class="card-text">You can find here all the informatins about students in the system</p>
+
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">CNE</th>
+                <th scope="col">First name</th>
+                <th scope="col">Second Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Speciality</th>
+                <th scope="col">Operations</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->cne }}</td>
+                    <td>{{ $student->firstName }}</td>
+                    <td>{{ $student->secondName }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->speciality }}</td>
+                    <td>
+
+                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+
+                    </td>
+
+
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
+
+
